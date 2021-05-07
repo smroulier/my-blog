b5973e718d17f046da9397cb9b17e811313091de
Sean Roulier <sean@seans-air.attlocal.net>
2021-05-07 16:47:00 -0500

    First commit

---
+++ src/build/precache-manifest.31782651b96e7a84109a479000643c6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6e78899200f9a8c0f7d",
+    "url": "/static/css/main.42c32268.chunk.css"
+  },
+  {
+    "revision": "e6e78899200f9a8c0f7d",
+    "url": "/static/js/main.a4a3ae1f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d72546995997ab1773b6",
+    "url": "/static/js/2.f96445f7.chunk.js"
+  },
+  {
+    "revision": "fd18e9dcb4df113265580d119fdf7df8",
+    "url": "/index.html"
+  }
+];
